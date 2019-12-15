bbc040820189148ff3cced5f213eb5749031311d
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2019-12-15 07:33:27 +0530

    Initial Commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <center>
+          MovieMap
+        </center>
+      </>
+    );
+  }
+}
+
+export default Home;
